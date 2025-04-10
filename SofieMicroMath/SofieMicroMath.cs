0ddee6bec4e7d8df4abc99d4cd45e01a8c7d6cfb
Sofie Kobbernagel Jensen <sofie.jensen.03@gmail.com>
2025-04-10 10:41:16 +0200

    add function comment added

--- SofieMicroMath/SofieMicroMath.cs
+++ SofieMicroMath/SofieMicroMath.cs
@@ -8,6 +8,13 @@ namespace SofieMicroMath
 {
     public class SofieMicroMath
     {
+
+        /// <summary>
+        /// Adds two numbers
+        /// </summary>
+        /// <param name="t1">First number</param>
+        /// <param name="t2">Second number</param>
+        /// <returns>the sum of the two numbers</returns>
         public int Add(int t1, int t2)
         {
             return t1 + t2;
